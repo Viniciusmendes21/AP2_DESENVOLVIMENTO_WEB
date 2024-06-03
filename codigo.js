93d71829d40abb4998fa080d0b2336566caf0b69
Viniciusmendes21 <101270160+Viniciusmendes21@users.noreply.github.com>
2024-06-03 15:38:47 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -2,18 +2,18 @@ import { hex_sha256 } from "./sha256-min.mjs";
 
 const alvo = "e521bd98978b370a013a7adff31b449bd5db13ec2962a54cb8feeaa254ab8618";
 
-document.
 
 document.getElementById('btn-logar').onclick = () => {
     const entrada = document.getElementById('senha').value;
-    console.log(entrada);
     
     if (hex_sha256(entrada) == alvo) {
         console.log('logado')
-
-        sessionStorage.setItem("estado",  "logado");
-        
+        sessionStorage.setItem("estado", "logado");
+        document.getElementById('tela-login').style.display = 'none'; // Esconde tela-login
+        document.getElementById('pos-login').style.display = 'block'; // Mostra pos-login
     } else {
         console.log('não logado')
+        sessionStorage.setItem("estado", "deslogado");
     }
 }
+
